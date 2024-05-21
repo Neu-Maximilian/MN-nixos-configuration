7e1044f39d39383c1f4552d0fd825f45d194a89e
Maximilian Neu <neu.maximilian@proton.me>
2024-05-21 22:44:54 +0200

    Update .gitignore include secret file, for vpn

---
+++ hosts/asus-laptop/custom/vpn.nix
@@ -0,0 +1,48 @@
+{ pkgs, ... }:
+let
+  # Import the TOML file
+  config = builtins.fromTOML (builtins.readFile "/etc/nixos/.secrets/wireguard-client.toml");
+in
+{
+  # Enable WireGuard
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the client's end of the tunnel interface.
+      # Get this from a secret file in .secrets/wireguard-client.toml
+      
+      ips = [ config.ips ];
+      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKey = config.privateKey;
+
+      peers = [
+        # For a client configuration, one peer entry for the server will suffice.
+
+        {
+          # Public key of the server (not a file path).
+          publicKey = config.publicKey;
+
+          # Pre-shared key
+          presharedKey = config.presharedKey;
+
+          # Forward all the traffic via VPN.
+          # allowedIPs = [ "0.0.0.0/0" ];
+          # Or forward only particular subnets
+          allowedIPs = config.allowedIPs;
+
+          # Set this to the server IP and port.
+          endpoint = config.endpoint; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+
+          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+}
