95ccebddc4df3cf20ab993ff79849d74f92bd6ef
Maximilian NEU <neu.maximilian@proton.me>
2024-06-06 13:12:10 +0200

    chore: Update package dependencies and add new programs

--- home/maximiliann/default.nix
+++ home/maximiliann/default.nix
@@ -28,45 +28,6 @@
   #  "Xcursor.size" = 16;
   #  "Xft.dpi" = 172;
   #};
-
-
-  # Packages that should be installed to the user profile.
-  home.packages = with pkgs; [
-
-    # System Administration
-    powertop      # see power statistics on intel-based laptops
-                  # run a calibration with "powertop --calibrate"
-		  # It will take 5-10 minutes
-		  # then let It run for an hour and get power
-		  # estimation figures
-    btop          # System monitor
-    ncdu          # Analyze disk usage 
-
-    # Media editing
-    audacity            # audio
-    libsForQt5.kdenlive # video editor
-    olive-editor        # new and promising video editor
-    obs-studio          # recording
-    gimp   # image editor
-    rawtherapee         # RAW image editor
-
-    # Gaming
-    steam
-    wineWowPackages.stable
-    winetricks
-    # Games
-    osu-lazer
-
-    # Graphics
-    mesa-demos          # opengl and lots of tests
-    
-    # Browsers
-    firefox
-    ungoogled-chromium
-
-    # Code editors
-    vscode-fhs
- ];
   
   nixpkgs.config.allowUnfree = true;
 
--- home/maximiliann/programs/default.nix
+++ home/maximiliann/programs/default.nix
@@ -3,4 +3,39 @@
 {
   imports = [
   ];
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+
+    # System Administration
+    powertop      # Power management
+    btop          # System monitor
+    ncdu          # Analyze disk usage 
+    virt-viewer   # Virtual machine viewer
+
+    # Media editing
+    audacity            # audio
+    libsForQt5.kdenlive # video editor
+    olive-editor        # new and promising video editor
+    obs-studio          # recording
+    gimp   # image editor
+    rawtherapee         # RAW image editor
+
+    # Gaming
+    steam
+    wineWowPackages.stable
+    winetricks
+    # Games
+    osu-lazer
+
+    # Graphics
+    mesa-demos          # opengl and lots of tests
+    
+    # Browsers
+    firefox
+    ungoogled-chromium
+
+    # Code editors
+    vscode-fhs
+ ];
 }
