02ba15756636ee05ecd47c6aa339c41cb2de76c1
Maximilian NEU <neu.maximilian@proton.me>
2024-06-07 21:27:23 +0200

    chore: Update build.sh to support rebuilding the bootloader

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 # Assign script arguments to variables
 home_config=$1
 system_config=$2
-verbose=$3
-bootloader=$4
+flags=$3
 
 # List of home configurations
 home_configs="maximiliann "
@@ -13,7 +12,7 @@ system_configs="bf-109 "
 
 # Check if both arguments are provided
 if [ -z "$home_config" ] || [ -z "$system_config" ]; then
-  echo "Usage: $0 <home_config> <system_config> [--verbose]"
+  echo "Usage: $0 <home_config> <system_config> [--verbose] [--rebuild-bootloader]"
   echo "Available system configurations: $system_configs"
   echo "Available home configurations: $home_configs"
   exit 1
@@ -53,16 +52,13 @@ sudo chown -R root:root /etc/nixos
 # Cd to /etc/nixos
 cd /etc/nixos
 
-# Check if both flags are provided, set an error message if both are provided
-if [ "$verbose" = "--verbose" ] && [ "$3" = "--rebuild-bootloader" ]; then
-  echo "Error: --verbose and --rebuild-bootloader flags cannot be used together."
-  exit 1
-fi
-
 # Rebuild the system configuration
-if [ "$verbose" = "--verbose" ]; then
-  echo "Rebuilding system configuration..."
+if [ "$flags" = "--verbose" ]; then
+  echo "Rebuilding system configuration... (verbose)"
   sudo nixos-rebuild switch --flake .#$system_config --show-trace --impure
+elif [ "$flags" = "--rebuild-bootloader" ]; then
+  echo "Rebuilding system configuration... (rebuild-bootloader)"
+  sudo nixos-rebuild switch --flake .#$system_config --impure
 else
   echo "Rebuilding system configuration..."
   sudo nixos-rebuild switch --flake .#$system_config --impure
@@ -71,12 +67,6 @@ fi
 # Switch to the home configuration
 echo "Switching to home configuration..."
 home-manager switch --flake .#$home_config
-
-# If the --bootloader flag is provided, rebuild the bootloader
-if [ "$bootloader" = "-bootloader" ]; then
-  echo "Rebuilding bootloader..."
-  sudo nixos-rebuild --install-bootloader boot --flake .#$system_config --impure
-fi
 
 # Exit with the exit code of the last command
 exit $?
--- hosts/shared/base/bootloader.nix
+++ hosts/shared/base/bootloader.nix
@@ -1,12 +1,21 @@
 { pkgs,  ... }:
 
 {
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = 2;
-  boot.initrd.enable = true;
-  boot.initrd.systemd.enable = true;
+  # Grub is the bootloader.
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+
+    grub = {
+      enable = true;
+      efiSupport = true;
+      useOSProber = true;
+    };
+  };
+
+  # Plymouth is a graphical boot animation.
   boot.plymouth = {
     enable = true;
     font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
